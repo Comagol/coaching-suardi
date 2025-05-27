d5d8c019132f5dbf8cd28d173c911eefb9db80a5
Comagol <comabellafrancisco@gmail.com>
2025-05-27 10:46:33 -0300

    adding cartcomtext to manage events

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
+import { CartProvider } from "./context/CartContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <CartProvider>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </CartProvider>
   </React.StrictMode>
 );
